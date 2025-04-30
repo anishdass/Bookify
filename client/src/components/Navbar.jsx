26e33fc41e856f08b330ceb1a0f90fe53f0963d4
anishdass <anishdassatoffice@gmail.com>
2025-04-30 13:57:11 +0100

    completed the landing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+
+const BookIcon = () => (
+  <svg
+    className='w-4 h-4 text-gray-700'
+    aria-hidden='true'
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    fill='none'
+    viewBox='0 0 24 24'>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4'
+    />
+  </svg>
+);
 
 const Navbar = () => {
   const navLinks = [
@@ -11,13 +31,21 @@ const Navbar = () => {
     { name: "About", path: "/" },
   ];
 
-  const [isScrolled, setIsScrolled] = React.useState(false);
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { openSignIn } = useClerk();
   const { user } = useUser();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  React.useEffect(() => {
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+    setIsScrolled((prev) => (location.pathname !== "/" ? true : false));
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 10);
     };
@@ -61,7 +89,8 @@ const Navbar = () => {
         <button
           className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
             isScrolled ? "text-black" : "text-white"
-          } transition-all`}>
+          } transition-all`}
+          onClick={() => navigate("/owner")}>
           Dashboard
         </button>
       </div>
@@ -73,15 +102,39 @@ const Navbar = () => {
           alt='search'
           className={`${isScrolled && "invert"}h-7 transition-all duration-500`}
         />
-        <button
-          onClick={openSignIn}
-          className='bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500'>
-          Login
-        </button>
+        {user ? (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label='My Bookings'
+                labelIcon={<BookIcon />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className='bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500'>
+            Login
+          </button>
+        )}
       </div>
 
       {/* Mobile Menu Button */}
+
       <div className='flex items-center gap-3 md:hidden'>
+        {user && (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label='My Bookings'
+                labelIcon={<BookIcon />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
         <img
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           src={assets.menuIcon}
@@ -107,13 +160,19 @@ const Navbar = () => {
           </a>
         ))}
 
-        <button className='border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all'>
-          Dashboard
-        </button>
+        {user && (
+          <button
+            className='border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all'
+            onClick={() => navigate("/owner")}>
+            Dashboard
+          </button>
+        )}
 
-        <button className='bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500'>
-          Login
-        </button>
+        {!user && (
+          <button className='bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500'>
+            Login
+          </button>
+        )}
       </div>
     </nav>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Hero from "../components/Hero";
+import FeaturedDestination from "../components/FeaturedDestination";
+import ExclusiveOffers from "../components/ExclusiveOffers";
+import Testimonials from "../components/Testimonials";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
     <div>
       <>
         <Hero />
+        <FeaturedDestination />
+        <ExclusiveOffers />
+        <Testimonials />
+        <NewsLetter />
       </>
     </div>
   );
